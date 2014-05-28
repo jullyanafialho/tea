787f720ff6b4225debda541b5e57d237c9f13168
rodrigocarfer <rodrigocarfer@gmail.com>
2014-05-27 19:15:47 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,34 +1,80 @@
+var rightAnswerId = -1;
+var correctAnswers = 0;
+
 function generateNumber(){
-	var a = Math.floor((Math.random() * 10) + 1);
+
+	var a = Math.floor((Math.random() * 10)+ 1);
 	return a;
+	
+}
+
+function generateWrongAnswers(rightAnswer){
+	
+	var allAnswers = [];
+	
+	while(allAnswers.length < 4){
+	
+	var addValue = generateNumber();
+	
+	var valueToBeAdded = 0;
+	
+	var signal = generateNumber();
+	
+		if( signal > 5)
+		
+			valueToBeAdded = rightAnswer + addValue;	
+			
+		else valueToBeAdded = rightAnswer - addValue;
+				
+		if(allAnswers.indexOf(valueToBeAdded) > -1 || valueToBeAdded <= 0)	
+			continue;
+		else allAnswers.push(valueToBeAdded);
+	
+	}
+	return allAnswers;
+
 }
 
 function writeQuestion(){
+
 	var a = generateNumber() ;
 	var b = generateNumber() ;
 	var x = a + "*" + b ;
  	document.getElementById("question").innerHTML = x;
  	return a*b;
+	
 }
 
 function answersQuestion(){
 
 	var rightAnswer = writeQuestion();
 
-	var answers = [3];
+	var answers = [];
 
-	for(var i=0;i<4;i++){
-
-		answers[i]=generateNumber();
-	}
-
-	var rightPosition = Math.floor((Math.random() * 10)%4)
+	answers = generateWrongAnswers(rightAnswer);
+	
+	var rightPosition = Math.floor((Math.random() * 16)/4);
+	
 	answers[rightPosition] = rightAnswer;
-
+	
+	rightAnswerId = rightPosition;
+	
 	for(var i=0;i<4;i++){
-
-		document.getElementById(Math.floor(i)).innerHTML = answers[i];
+		document.getElementById(i).innerHTML = answers[i];
 	}
 
+}
+
+function onOptionClick(id){
+
+	if(id != rightAnswerId)
+		alert("Wrong answer!");
+	else {
+	
+	correctAnswers++;
+	answersQuestion();
+	document.getElementById("correctAnswers").innerHTML = "Correct Answers: "+correctAnswers;
+	
+	}  
 
 }
